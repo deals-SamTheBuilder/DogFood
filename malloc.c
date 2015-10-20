aeea0837a6b402431f00f493c8659f3d6b25e598
AnSwErYWJ <ywj1993@yeah.net>
2015-10-20 23:16:21 +0800

    动态分配内存

--- malloc.c
+++ malloc.c
@@ -10,32 +10,59 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
+/*
+ * 方法一：使用malloc
+ */
 void *MALLOC(size_t size)
 {
     void *men;
-    men=(void *)malloc(size);//类型指派可选
+    men = (void *)malloc(size);//类型指派可选
     if(men == NULL)
     {
         fprintf(stderr,"Malloc failed!\n");
         exit(1);
     }
+    
+    memset(men,'\0',size);
+
+    return men;
+}
+
+/*
+ * 方法二：使用calloc
+ */
+void *CALLOC(size_t n,size_t size)
+{
+    void *men;
+    men = (void *)calloc(n,size);//类型指派可选
+    if(men == NULL)
+    {
+        fprintf(stderr,"Malloc failed!\n");
+        exit(1);
+    }
+    
     return men;
 }
 
 void FREE(void *men)
 {
     free(men);
-    men=NULL;
+    men = NULL;
 }
 
 int main(void)
 {
     int *array;
-
-    array=(int *)MALLOC(8*sizeof(int));
-    /*do sth*/
-     FREE(array);
+    char *str;
     
+    array = (int *)MALLOC(8*sizeof(int));
+    str = (char *)CALLOC(8,sizeof(char));
+    
+    /*do sth*/
+    FREE(array);
+    FREE(str);
+
     return 0;
 }
