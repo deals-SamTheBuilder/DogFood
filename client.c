3ba3184fb3fbb18cc633aba814119a23204caa94
WeiJie Yuan <1045837697@qq.com>
2016-04-26 17:39:44 +0800

    non-blocking connect

--- client.c
+++ client.c
@@ -1,13 +1,9 @@
 /*************************************************************************
-	> File Name: client.c
-	> Author: AnSwEr
-	> Mail: 1045837697@qq.com
-	> Created Time: 2015年08月13日 星期四 10时57分50秒
- ************************************************************************/
-
-/*
- * This is my client  
- */
+> File Name: client.c
+> Author: WeiJieYuan
+> Mail: 
+> Created Time: Fri 11 Mar 2016 09:18:42 AM CST
+************************************************************************/
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -17,153 +13,189 @@
 #include<strings.h>
 #include<unistd.h>
 #include<arpa/inet.h>
+#include<netdb.h>
+#include<string.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<errno.h>
 
-#define PORT 8000
 #define MAXSLEEP 128
+#define PORT 80
+#define MAXSIZE 4096
 
-/*
- * debug log function
- */
-#define DEBUG_PRINT 1
-#if defined(DEBUG_PRINT)
-#define DEBUG(...)\
-        do{\
-           fprintf(stderr,"-----DEBUG-----\n");\
-           fprintf(stderr,"%s,%s\n",__TIME__,__DATE__);\
-           fprintf(stderr,"%s:%d:%s():",__FILE__,__LINE__,__func__);\
-           fprintf(stderr,__VA_ARGS__);\
-        }while(0)
-#endif
-
-static int fd,ret;
 static struct sockaddr_in servaddr;
+static int ret,fd;
 
-int client_init(int port ,const char *ip)
+/*Set client opt*/
+int client_init(const unsigned short int port,const char *hostname)
 {
-    int sockfd,ret;
+    int sockfd;
+    struct hostent *h;
+    char ip[16];
 
-    sockfd = socket(PF_INET,SOCK_STREAM,0);
-    if(sockfd < 0)
+    if((sockfd = socket(PF_INET,SOCK_STREAM,0)) < 0)
     {
-        perror("clinet socket error");
-        return -1;
+        perror("Socket Error");
+        exit(EXIT_FAILURE);
+    }
+
+    int flags = fcntl(sockfd,F_GETFL,0);
+    if(fcntl(sockfd,F_SETFL,flags|O_NONBLOCK) < 0)
+    {
+        fprintf(stderr,"Error:fcntl failed!");
+        exit(EXIT_FAILURE);
     }
 
     bzero(&servaddr,sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port=htons(port);
-    ret = inet_pton(AF_INET,ip,&servaddr.sin_addr);
-    if(ret != 1)
+    servaddr.sin_port = htons(port);
+
+    if((h = gethostbyname(hostname)) == NULL)
     {
-        DEBUG("<IPAddress>:xxx.xxx.xxx.xxx\n");
-        return -2;
+        fprintf(stderr,"Gethostbyname error for host:%s\n",hostname);
+        exit(EXIT_FAILURE);
     }
+    inet_ntop(h->h_addrtype,h->h_addr,ip,sizeof(ip));
+    printf("%s's ip is %s\n",hostname,ip);
+    inet_pton(h->h_addrtype,ip,&servaddr.sin_addr);
 
     return sockfd;
 }
 
-int my_heartbeat(int fd)
-{
-    int alive,error,idle,cnt,intv;
-
-    /*
-     * open keepalive on fd
-     */
-    Restart:
-    alive = 1;//set keepalive open
-    ret=setsockopt(fd,SOL_SOCKET,SO_KEEPALIVE,&alive,sizeof(alive));
-    if(ret < 0)
-    {
-        DEBUG("set socket option error.\n");
-        goto Restart;
-    }
-    
-    /*
-     * 60S without data,send heartbeat package
-     */
-    idle = 30;
-    ret = setsockopt(fd,SOL_TCP,TCP_KEEPIDLE,&idle,sizeof(idle));
-    if(ret < 0)
-    {
-        DEBUG("set keepalive idle error.\n");
-        return -1;
-    }
-
-    /*
-     * without any respond,3m later resend package
-     */
-    intv = 180;
-    ret = setsockopt(fd,SOL_TCP,TCP_KEEPINTVL,&intv,sizeof(intv));
-    if(ret < 0)
-    {
-        DEBUG("set keepalive intv error.\n");
-        return -2;
-    }
-
-    /*
-     * send 5 times,without any response,mean connect lose 
-     */
-    cnt = 5;
-    ret = setsockopt(fd,SOL_TCP,TCP_KEEPCNT,&cnt,sizeof(cnt));
-    if(ret < 0)
-    {
-        DEBUG("set keepalive cnt error.\n");
-        return -3;
-    }
-}
-
- int my_connect(int sockfd,const struct sockaddr *servaddr,socklen_t addrlen)
+int my_connect(int sockfd,const struct sockaddr *servaddr,socklen_t addrlen)
 {
     int nsec;
     for(nsec = 1;nsec <= MAXSLEEP;nsec <<= 1)
     {
         if(connect(sockfd,servaddr,addrlen) == 0)
-            return 0;//connection accepted
-        if(nsec <= MAXSLEEP/2)//sleep nesc,then connect retry
-            sleep(nsec);
+        return 0;
+        if(nsec <= MAXSLEEP/2)
+        sleep(nsec);
     }
-    return -1;
+    return -1;   
 }
 
-int main(int argc,const char *argv[])
+int main(int argc ,const char *argv[])
 {
-    
+
     if(argc != 2)
     {
-        DEBUG("Usage: run <IPAdress>\n");
-        exit(1);
+        fprintf(stderr,"Usage: Please input ServerName!\n");
+        exit(EXIT_FAILURE);
     }
 
-    /*
-     * socket communication
-     */
-    fd = client_init(PORT,argv[1]);
-    if(fd < 0)
+    if((fd = client_init(PORT,argv[1])) < 0)
     {
-        DEBUG("client_init error.\n");
-        exit(2);
+        fprintf(stderr,"Client Init Error\n");
+        exit(EXIT_FAILURE);
     }
 
-    ret = my_connect(fd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+    ret = connect(fd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+    fd_set rset,wset;
+    FD_ZERO(&rset);
+    FD_ZERO(&wset);
+    FD_SET(fd,&rset);
+    FD_SET(fd,&wset);
+
+
+    if(ret == 0)
+        printf("Connect succeed!\n");
+    else
+    {
+        if(errno == EINPROGRESS)
+        {
+            if(select(fd+1,&rset,&wset,NULL,NULL) < 1)
+            {
+                fprintf(stderr,"Select Error\n");
+                exit(EXIT_FAILURE);
+            }
+            if(FD_ISSET(fd,&rset) && FD_ISSET(fd,&wset))
+            {
+                int error = 0;
+                socklen_t len = sizeof(error);
+                if(getsockopt(fd,SOL_SOCKET,SO_ERROR,&error,&len)<0)
+                    exit(EXIT_FAILURE);
+                printf("error = %d\n",error);
+                if(error != 0)// not connect
+                {
+                    fprintf(stderr,"Error:Connect failed\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            if((!FD_ISSET(fd,&rset)) && FD_ISSET(fd,&wset))
+                printf("Connect succeed!!\n");
+        }
+        else
+        {
+            fprintf(stderr,"Error:Connect failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+   
+    char http_req[1024];
+    memset(http_req,0,sizeof(char)*1024);
+    sprintf(http_req,"GET /index.html HTTP/1.1\nHost:%s\nConnection:close\n\n",argv[1]);
+
+
+    ret = send(fd,http_req,strlen(http_req)+1,0);
     if(ret < 0)
     {
-        DEBUG("connect error.\n");
-        exit(3);
+        fprintf(stderr,"Send Error\n");
+        exit(EXIT_FAILURE);
     }
-
-    ret = my_heartbeat(fd);
-    if(ret < 0)
+    printf("Send succeed\n");
+    
+    FILE *fp = fopen("response.txt","wb");
+    if(fp == NULL)
     {
-        DEBUG("heartbeat error.\n");
-        exit(4);
+        fprintf(stderr,"File Open Error\n");
+        exit(EXIT_FAILURE);
     }
-
+    
     while(1)
     {
-        //do sth
+        char http_res[MAXSIZE];
+        memset(http_res,0,sizeof(char)*MAXSIZE);
+
+        FD_ZERO(&rset);
+        FD_ZERO(&wset);
+        FD_SET(fd,&rset);
+
+        if(select(fd+1,&rset,NULL,NULL,NULL) < 1)
+        {
+            fprintf(stderr,"Select Error\n");
+            exit(EXIT_FAILURE);
+        }
+        
+        if(FD_ISSET(fd,&rset))
+        {
+            ret = recv(fd,http_res,sizeof(char)*MAXSIZE,0);
+
+            /*receive error*/
+            if(ret < 0)
+            {
+                /*no data in buffer*/
+                if(errno == EAGAIN || errno == EINTR || errno == EWOULDBLOCK)
+                    continue;
+                else
+                {
+                    fprintf(stderr,"RECV ERROR\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            /*receive over,server close the connection*/
+            if(ret == 0)
+            {
+                fprintf(stderr,"Server close the connection\n");
+                break;
+            }
+            fputs(http_res,fp);
+        }
     }
 
+    printf("Write done\n");
+    fclose(fp);
     close(fd);
-
     return 0;
 }
