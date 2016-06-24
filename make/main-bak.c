20c3b07e9e0665f631f74b48444a6b07d977645f
AnSwErYWJ <ywj1993@yeah.net>
2016-06-24 15:18:45 +0800

    make

---
+++ make/main-bak.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: main-bak.c
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 24 Jun 2016 02:01:54 PM CST
+ ************************************************************************/
+
+#include"print.h"
+
+int main(int argc,const char *argv[])
+{
+    print();    
+}
+
