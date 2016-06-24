b0a414ac25db39671d05a95512bbcf86d61f0d48
AnSwErYWJ <ywj1993@yeah.net>
2016-06-24 12:02:41 +0800

    make

---
+++ make/print.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: print.c
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 24 Jun 2016 11:26:50 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+#include"print.h"
+
+void print(void)
+{
+    printf("Hello,world!\n");
+}
