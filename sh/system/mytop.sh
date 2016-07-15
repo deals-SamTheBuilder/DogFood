143ba72f7e8f58cdc241c99e2ca9c8d8631e1fe4
AnSwErYWJ <ywj1993@yeah.net>
2016-07-15 09:53:46 +0800

    mytop

--- sh/system/mytop.sh
+++ sh/system/mytop.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+# top your program
 
-top -p `pidof $1`
+pids=`pidof $1`
+#echo -e ${pids}
+
+pids=`echo -e ${pids} | sed 's/\s/,/g'`
+#echo -e ${pids}
+
+top -p ${pids}
+
+if (($? != 0));then
+    echo -e "Usage: sh mytop.sh [program]"
+fi
