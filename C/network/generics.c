00e613d2aa306dbb2e1783660603be4e3d64c11f
AnSwErYWJ <ywj1993@yeah.net>
2016-10-08 17:41:59 +0800

    generics for Unix Network Program

---
+++ C/network/generics.c
@@ -0,0 +1,9 @@
+/*************************************************************************
+	> File Name: generics.c
+	> Author: weijie.yuan
+	> Mail: yuanweijie1993@gmail.com
+	> Created Time: Sat 08 Oct 2016 05:40:48 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
