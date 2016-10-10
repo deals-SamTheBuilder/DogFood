f664e849bcb6118d2edb23b47a7eb1a23742e2b4
AnSwErYWJ <ywj1993@yeah.net>
2016-10-10 10:58:12 +0800

    Linux .a and .so

---
+++ C/so/welcome.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: welcome.h
+	> Author: weijie.yuan
+	> Mail: yuanweijie1993@gmail.com
+	> Created Time: Mon 10 Oct 2016 10:24:37 AM CST
+ ************************************************************************/
+
+#ifndef _WELCOME_H
+#define _WELCOME_H
+
+void welcome();
+
+#endif
