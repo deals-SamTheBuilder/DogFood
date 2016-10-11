aa90ff591374d3bfe64055396d5709c232698f73
AnSwErYWJ <ywj1993@yeah.net>
2016-10-11 10:10:53 +0800

    support -D

--- make/print.c
+++ make/print.c
@@ -5,6 +5,8 @@
 	> Created Time: Fri 24 Jun 2016 11:26:50 AM CST
  ************************************************************************/
 
+#ifdef __ANSWER__
+
 #include<stdio.h>
 #include"print.h"
 
@@ -12,3 +14,5 @@ void print(void)
 {
     printf("Hello,world!\n");
 }
+
+#endif
