8dc1779b68cde095f721ea75ce2a41b830652fd3
AnSwErYWJ <ywj1993@yeah.net>
2016-11-21 11:58:00 +0800

    通道转换

--- C/audio-tools/channel_convert.c
+++ C/audio-tools/channel_convert.c
@@ -1,5 +1,5 @@
 /*************************************************************************
-	> File Name: 1to3.c
+	> File Name: channel_convert.c
 	> Author: weijie.yuan
 	> Mail: yuanweijie1993@gmail.com
 	> Created Time: Wed 16 Nov 2016 10:08:06 AM CST
@@ -7,10 +7,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-
-#define OUT_FILE "three_channels.pcm"
-#define RAW_PASS 8
-#define NEW_PASS 3
+#include<string.h>
 
 void *my_calloc(size_t n,size_t size)
 {
@@ -19,7 +16,7 @@ void *my_calloc(size_t n,size_t size)
     men = (void *)calloc(n,size);
     if(men == NULL)
     {
-        fprintf(stderr,"calloc failed!\n");
+        fprintf(stderr,"Error:calloc failed!\n");
         exit(EXIT_FAILURE);
     }
     
@@ -35,49 +32,80 @@ void my_free(void *men)
     }
 }
 
-
-int main(int argc,char *argv[])
+int main(int argc, const char *argv[])
 {
-    if(argc != 2)
+    if(argc != 5)
     {
-        fprintf(stderr,"Usage:Please choose the input file!");
+        fprintf(stderr,"Usage: channel_convert input_channel(s) input_file output_channel(s) output_file\n");
         exit(EXIT_FAILURE);
     }
+    
+    int raw = atoi(argv[1]);
+    int new = atoi(argv[3]);
+    if (raw < 0 || new < 0)
+    {
+        fprintf(stderr,"Error: channel(s) must be positive number");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Running message:\n  input_file:%s\n  input_channel(s):%d\n  output_file:%s\n  output_channel(s):%d\n\n",argv[2],raw,argv[4],new);
+    
+    /* channel judge */
+    if (raw >= new)
+    {
+        printf("Action: cut audio data\n");
+    }
+    else if(raw != 1 || new > 16)
+    {
+        fprintf(stderr,"Error: only one channel data can be synthetised to multichannel(max is 16) audio data\n");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        printf("Action: synthetise audio data\n");
+    }
 
-    FILE *in = fopen(argv[1],"rb");
+    /* open file */
+    FILE *in = fopen(argv[2],"rb");
     if(in == NULL)
     {
-        fprintf(stderr,"Error:%s open failed!",argv[1]);
+        fprintf(stderr,"Error:%s open failed!\n",argv[2]);
         exit(EXIT_FAILURE);
     }
 
-    FILE *out = fopen(OUT_FILE,"wb");
+    FILE *out = fopen(argv[4],"wb");
     if(out == NULL)
     {
-        fprintf(stderr,"Error:%s open failed!",OUT_FILE);
-        exit(3);
+        fprintf(stderr,"Error:%s open failed!\n",argv[4]);
+        exit(EXIT_FAILURE);
     }
 
-    char *in_buf = (char *)my_calloc(1,2*RAW_PASS);
-    char *out_buf = (char *)my_calloc(1,2*NEW_PASS);
+    int frame_len = sizeof(short int);
+    short int *in_buf = (short int *)my_calloc(raw,frame_len);
+    short int *out_buf = (short int *)my_calloc(new,frame_len);
     
-    int ret = 0;
     int i = 0;
     
     while(1)
     {
-        if(fread(in_buf,1,2*RAW_PASS,in) != 2*RAW_PASS)
+        if(fread(in_buf,frame_len,raw,in) != raw)
             break;
-        for(i = 0;i <= 2*NEW_PASS-1;i++)
+
+        for(i = 0;i <= new;i++)
         {
-            if(i<=2*(NEW_PASS-1)-1)
-                out_buf[i] = in_buf[i];
+            if (raw == 1)
+                out_buf[i] = in_buf[0];
             else
-                out_buf[i] = in_buf[i]; // eight -> seven 
+                out_buf[i] = in_buf[i];
         } 
-        fwrite(out_buf,1,2*NEW_PASS,out);
+        
+        fwrite(out_buf,frame_len,new,out);
+        memset(in_buf, 0, raw*frame_len);
+        memset(out_buf, 0, new*frame_len);
     }
-    
+   
+    printf("\nSuccess\n");
+
     my_free(in_buf);
     my_free(out_buf);
     fclose(in);
